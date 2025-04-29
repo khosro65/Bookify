1cdbcf3d908aab902feab38fedee7e9d3d97e8ae
Khosro <khosro.dav@gmail.com>
2025-04-29 15:44:09 +0330

    add DI in application layer

---
+++ Bookify.Application/DependencyInjections.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Application;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjections).Assembly);
+        });
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
