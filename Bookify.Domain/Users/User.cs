a4127659b13d58f9c163ed9f4569e1d3aca332b0
Khosro <khosro.dav@gmail.com>
2025-04-29 11:33:26 +0330

    add user created domain event

--- Bookify.Domain/Users/User.cs
+++ Bookify.Domain/Users/User.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
 
 namespace Bookify.Domain.Users;
 
@@ -24,7 +25,9 @@ public class User: Entity
 
     public User Create(FirstName firstName, LastName lastName, Email email)
     {
-        var user = new User(Guid.NewGuid(), firstName, lastName, email); 
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+        user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
 
         return user;
     }
