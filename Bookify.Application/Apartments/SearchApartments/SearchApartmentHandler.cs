90d4b85907610d0b273168dbf26f93260dddd1c9
Khosro <khosro.dav@gmail.com>
2025-04-29 18:31:23 +0330

    implement booking and apartment queries

---
+++ Bookify.Application/Apartments/SearchApartments/SearchApartmentHandler.cs
@@ -0,0 +1,64 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Dapper;
+using MediatR;
+
+namespace Bookify.Application.Apartments.SearchApartments;
+
+internal sealed class SearchApartmentHandler : IQueryHandler<SearchApartmentQuery, IReadOnlyList<ApartmentResponse>>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+    private static readonly int[] ActiveBookingStatuses =
+        [
+            (int)BookingStatus.Reserved,
+            (int)BookingStatus.Confirmed,
+            (int)BookingStatus.Completed,
+        ];
+    public SearchApartmentHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+    public async Task<Result<IReadOnlyList<ApartmentResponse>>> Handle(SearchApartmentQuery request, CancellationToken cancellationToken)
+    {
+        if (request.StartDate > request.EndDate)
+            return new List<ApartmentResponse>();
+
+
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string sql =
+            """
+            SELECT * FROM Apartments a 
+            WHERE NOT EXISTS
+            (
+                SELECT 1 FROM Bookings as b
+                WHERE
+                     b.Id = a.Id AND
+                     b.StartDate <= @EndDate AND
+                     b.EndDate >= @StartDate AND
+                     b.Status in (@ActiveBookingStatuses)
+            )
+            """;
+
+        var apartments = await connection.QueryAsync<ApartmentResponse , AddressResponse , ApartmentResponse>(
+            sql, 
+            (apartment, address) =>
+            {
+                apartment.Address = address;
+
+                return apartment;
+            }
+            ,new
+            {
+                StartDate = request.StartDate,
+                EndDate = request.EndDate,
+                ActiveBookingStatuses
+            },
+            splitOn: "Country"
+            );
+
+        return apartments.ToList();
+    }
+}
---
+++ Bookify.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Dapper;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, BookingResponse>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetBookingQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+    public async Task<Result<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+
+        using var conneciton = _sqlConnectionFactory.CreateConnection();
+
+        const string sql =
+            """
+            SELECT * FROM Bookings WHERE Id = @Id
+            """;
+
+        var booking = await conneciton.QueryFirstOrDefaultAsync<BookingResponse>(
+            sql,
+            new { Id = request.BookingId });
+
+        return booking;
+    }
+}
