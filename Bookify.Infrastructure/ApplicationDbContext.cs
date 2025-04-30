c5400370d87299708c0c81eda20da4fdaafa0b25
Khosro <khosro.dav@gmail.com>
2025-04-30 18:18:53 +0330

    apply entity configurations on model creating

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -10,4 +10,10 @@ public sealed class ApplicationDbContext : DbContext , IUnitOfWork
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
