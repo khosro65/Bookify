fffd18937d6add064bbea6b38d51f5d3d205c079
Khosro <khosro.dav@gmail.com>
2025-05-02 18:19:24 +0330

    add global exception handle middleware

---
+++ Bookify.Api/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,74 @@
+﻿
+using Bookify.Application.Abstractions.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Middleware;
+
+public class GlobalExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger _logger;
+    public GlobalExceptionHandlingMiddleware(ILogger logger)
+    {
+        _logger = logger;
+    }
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+
+            _logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+            var exceptionDetails = GetExceptionDetails(exception);
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = exceptionDetails.Status,
+                Type = exceptionDetails.Type,
+                Title = exceptionDetails.Title,
+                Detail = exceptionDetails.Detail,
+            };
+
+            if (exceptionDetails.Errors is not null)
+            {
+                problemDetails.Extensions["errors"] = exceptionDetails.Errors;
+            }
+
+            context.Response.StatusCode = exceptionDetails.Status;
+
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+
+    private static ExceptionDetails GetExceptionDetails(Exception exception)
+    {
+        return exception switch
+        {
+            ValidationException validationException => new ExceptionDetails(
+                Status: StatusCodes.Status400BadRequest,
+                Type: "ValidationFailure",
+                Title: "Validation Error",
+                Detail: "One or more validation errors has occurred",
+                Errors: validationException.Errors
+                ),
+
+            _ => new ExceptionDetails(
+                Status: StatusCodes.Status400BadRequest,
+                Type: "ServerError",
+                Title: "Server error",
+                Detail: "An unexpected error has occurred",
+                Errors: null
+                ),
+        };
+    }
+
+    internal record ExceptionDetails(
+        int Status,
+        string Type,
+        string Title,
+        string Detail,
+        IEnumerable<object>? Errors);
+}
